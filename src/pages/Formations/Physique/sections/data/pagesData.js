825cac11a2aa5de372514fa3e01ae8d0b074c86c
Ibrahim laklaa <120943267+X1-Il@users.noreply.github.com>
2023-06-30 15:35:27 +0100

    Add files via upload

---
+++ src/pages/Formations/Physique/sections/data/pagesData.js
@@ -0,0 +1,33 @@
+/*
+=========================================================
+* e_ React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 X1-Il (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+import Hinaoui from "assets/images/Hinaoui.jpg";
+import Bouhamza from "assets/images/Bouhamza.jpg";
+import Ibraiken from "assets/images/Ibraiken.jpg";
+export default [
+  {
+    image: Hinaoui,
+    name: "Mohamed hianoui",
+    route: "/pages/landing-pages/about-us",
+  },
+  {
+    image: Bouhamza,
+    name: "Rachid Bouhamza",
+    route: "/pages/landing-pages/contact-us",
+  },
+  {
+    image: Ibraiken,
+    name: "Brahim Ibraiken",
+    route: "/pages/landing-pages/contact-us",
+  },
+];
