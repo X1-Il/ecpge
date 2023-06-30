765aef780dbe2f1685ebddae00cb0b88bf5b11a9
Ibrahim laklaa <120943267+X1-Il@users.noreply.github.com>
2023-06-30 15:41:54 +0100

    Add files via upload

---
+++ src/pages/Cours/Sup/Math/Cours/Sup-M4/C4.js
@@ -0,0 +1,45 @@
+/*
+=========================================================
+* e_ One_ - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 X1-Il (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Sections components
+import BaseLayout from "layouts/sections/components/BaseLayout";
+import View from "layouts/sections/components/View";
+//import C02 from "assets/Pdf/Sup/Math/Cours/C02.pdf";
+
+// Features page components
+import Pdf from "pages/Tech/Pdf/PdfViewer";
+import React from "react";
+import PropTypes from "prop-types";
+// Features page components code
+
+// eslint-disable-next-line react/prop-types
+const C4 = ({ C, label1, label2 }) => {
+  return (
+    <BaseLayout
+      title="Cours"
+      breadcrumb={[{ label: label1, route: "/Tech/Pdf" }, { label: label2 }]}
+    >
+      <View title="">
+        <Pdf pdfUrl={C} />
+      </View>
+    </BaseLayout>
+  );
+};
+Pdf.propTypes = {
+  C: PropTypes.string.isRequired,
+  label1: PropTypes.string.isRequired,
+  label2: PropTypes.string.isRequired,
+};
+export default C4;
